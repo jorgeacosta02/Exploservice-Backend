05cb3a7eca9c8fea7defa2e852f34cec8f3d843e
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-01-21 13:19:46 -0300

    zod-validations

---
+++ api/src/validations/auth.schemas.ts
@@ -0,0 +1,21 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+    username: z.string({
+        required_error: 'El usuario es requerido'
+    }),
+    email: z
+        .string({
+            required_error: 'El mail es requerido'
+        })
+        .email({
+            message: 'Email inválido'
+        }),
+    password: z
+        .string({
+            required_error: 'La constraseña es reqierida'
+        })
+        .min(6, {
+            message: 'La constraseña debe tener al menos 6 caracteres.'
+    }),
+});
