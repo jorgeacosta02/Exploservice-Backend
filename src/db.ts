a9a1b0f5f108ae4bbc6d0f455d63282b31646802
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-03-02 13:37:10 -0300

    postLocatonController

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,8 @@
 import { Sequelize } from 'sequelize-typescript';
 import { UserModel } from './models/UserModel';
 import { TaskModel } from './models/TaskModel';
+import { LocationModel } from './models/locationModel';
+import { ElementModel } from './models/elementModel';
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
@@ -8,7 +10,7 @@ const sequelize = new Sequelize({
   username: 'postgres',
   password: 'admin',
   database: 'exploservice',
-  models: [UserModel, TaskModel],
+  models: [UserModel, TaskModel, LocationModel, ElementModel],
 });
 
 export default sequelize;
